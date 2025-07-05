dae217261e4e05296dca535f2b4143253951bf49
wm337708@gmail.com <wm337708@gmail.com>
2025-07-05 20:38:24 +0800

    update views

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -159,7 +159,7 @@
                     <!--begin::Heading-->
                     <div class="text-center mb-5 mb-lg-10 py-10 py-lg-20">
                         <!--begin::Title-->
-                        <h1 class="text-white lh-base fw-bold fs-2x fs-lg-3x mb-15">SELAMAT DATANG
+                        <h1 class="lh-base fw-bold fs-2x fs-lg-3x mb-15" style= "color: white">SELAMAT DATANG
                             <br />Di
                             <span
                                 style="background: linear-gradient(to right, #0093dd 0%, #95ffff 100%);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">
